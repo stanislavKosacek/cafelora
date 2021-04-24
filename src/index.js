5cbef666e2e12cd217e50fb7cfd2f94fded18dc2
Stanislav Kosáček <stanislav.kosacek@qop.cz>
2021-04-24 11:40:32 +0200

    gallery text abbreviation, add script for hambureger menu

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 import './style.css';
 
-console.log('funguju!');
+const navElm = document.querySelector('nav');
+
+document.querySelector('#nav-btn').addEventListener('click', () => {
+  navElm.classList.toggle('nav-closed');
+});
+
+const navItems = navElm.querySelectorAll('a');
+
+navItems.forEach((navItem) => {
+  navItem.addEventListener('click', () => {
+    navElm.classList.add('nav-closed');
+  });
+});
