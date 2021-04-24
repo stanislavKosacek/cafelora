57e806469fcba7f6dc2687596b3c1c233265f24b
Stanislav Kosáček <stanislav.kosacek@qop.cz>
2021-04-24 12:18:32 +0200

    make drink orderable

--- src/index.js
+++ src/index.js
@@ -13,3 +13,14 @@ navItems.forEach((navItem) => {
     navElm.classList.add('nav-closed');
   });
 });
+
+document.querySelector('.order-btn').addEventListener('click', (e) => {
+  const drinkCupElm = document.querySelector('.drink__cup');
+  if (!drinkCupElm.classList.contains('drink__cup--selected')) {
+    drinkCupElm.classList.add('drink__cup--selected');
+    e.target.textContent = 'Zrušit';
+  } else {
+    drinkCupElm.classList.remove('drink__cup--selected');
+    e.target.textContent = 'Objednat';
+  }
+});
